22397774ab963b4cb1cb86a320cbf6bf17f4a830
tolusolaadeyemi <tolusa@hotmail.com>
2022-04-27 16:16:53 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'SMERP Theme',
+    'description': 'SMERP website as an Odoo theme',
+    'version': '1.0',
+    'author': 'Toluwanimi Sola-Adeyemi',
+    'category': 'Theme/Smerp',
+    'depends': ['website','website_theme_install',],
+    'data': [
+                'views/assets.xml',
+                'views/snippets.xml',
+                'views/layout.xml',
+                'views/homepage.xml',
+                'views/about.xml',
+                'views/faqs.xml',
+                'views/blog.xml',
+                'views/blogpost1.xml',
+                'views/blogpost2.xml',
+                'views/blogpost3.xml', ],
+    'application': True,
+}
+
+
